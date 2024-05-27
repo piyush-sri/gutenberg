19d3e036d2efc19c8cb3e7a02e895d8e0f9e40dd
Jorge Costa <jorge.costa@developer.pt>
2024-05-27 16:31:14 +0100

    Update: Move template areas into a panel.

--- packages/editor/src/components/sidebar/post-summary.js
+++ packages/editor/src/components/sidebar/post-summary.js
@@ -29,7 +29,6 @@ import BlogTitle from '../blog-title';
 import PostsPerPage from '../posts-per-page';
 import SiteDiscussion from '../site-discussion';
 import { store as editorStore } from '../../store';
-import TemplateAreas from '../template-areas';
 import { PrivatePostLastRevision } from '../post-last-revision';
 
 /**
@@ -86,7 +85,6 @@ export default function PostSummary( { onActionPerformed } ) {
 										<PostFormatPanel />
 										<PostStickyPanel />
 									</VStack>
-									<TemplateAreas />
 									{ fills }
 								</VStack>
 							) }
