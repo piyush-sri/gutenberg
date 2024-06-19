09b9d32125e1308cd8b1d9cb8a2990a4f816534e
Ben Dwyer <ben@scruffian.com>
2024-06-19 12:42:00 +0100

    update typesets to contain font families

--- packages/edit-site/src/components/global-styles/screen-typography.js
+++ packages/edit-site/src/components/global-styles/screen-typography.js
@@ -3,24 +3,15 @@
  */
 import { __ } from '@wordpress/i18n';
 import { __experimentalVStack as VStack } from '@wordpress/components';
-import { store as editorStore } from '@wordpress/editor';
-import { useSelect } from '@wordpress/data';
 
 /**
  * Internal dependencies
  */
 import TypographyElements from './typography-elements';
-import FontFamilies from './font-families';
 import ScreenHeader from './header';
 import TypesetButton from './typeset-button';
 
 function ScreenTypography() {
-	const fontLibraryEnabled = useSelect(
-		( select ) =>
-			select( editorStore ).getEditorSettings().fontLibraryEnabled,
-		[]
-	);
-
 	return (
 		<>
 			<ScreenHeader
@@ -32,8 +23,6 @@ function ScreenTypography() {
 			<div className="edit-site-global-styles-screen">
 				<VStack spacing={ 7 }>
 					<TypesetButton />
-					{ ! window.__experimentalDisableFontLibrary &&
-						fontLibraryEnabled && <FontFamilies /> }
 					<TypographyElements />
 				</VStack>
 			</div>
